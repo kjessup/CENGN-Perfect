75968f2304a6aae3fff2a344ae8a6e315286de45
Kyle Jessup <kyle.jessup@treefrog.ca>
2018-10-11 17:08:32 -0400

    Adding frameworks

---
+++ Server/djangoserver/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kjessup/development/TreeFrog/CENGN-Perfect/Server/djangoserver/env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
