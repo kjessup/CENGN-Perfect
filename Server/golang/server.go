3d940ba5daaeb783044c3ba8caca6c0d4a93b598
Kyle Jessup <kyle.jessup@treefrog.ca>
2018-10-15 18:57:21 -0400

    starter test plan

--- Server/golang/server.go
+++ Server/golang/server.go
@@ -1,16 +1,32 @@
 package main
 
 import (
-    "fmt"
     "log"
     "net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+func Repeat(s string, count int) string {
+    b := make([]byte, len(s)*count)
+    bp := copy(b, s)
+    for bp < len(b) {
+        copy(b[bp:], b[:bp])
+        bp *= 2
+    }
+    return string(b)
+}
+
+var s1024 = []byte(Repeat("A", 1024))
+
+func handlerEmpty(w http.ResponseWriter, r *http.Request) {
+    
+}
+
+func handler1024(w http.ResponseWriter, r *http.Request) {
+    w.Write(s1024)
 }
 
 func main() {
-    http.HandleFunc("/", handler)
+    http.HandleFunc("/empty", handlerEmpty)
+    http.HandleFunc("/1024", handler1024)
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
