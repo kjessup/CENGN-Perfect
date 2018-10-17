656cd4734d6673cc731ce97e165fd17a09e05a63
Kyle Jessup <kyle.jessup@treefrog.ca>
2018-10-16 21:08:24 -0400

    Changed exe name

--- Server/perfect/Package.swift
+++ Server/perfect/Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
 	name: "PerfectTemplate",
 	products: [
-		.executable(name: "PerfectTemplate", targets: ["PerfectTemplate"])
+		.executable(name: "perfect", targets: ["PerfectTemplate"])
 	],
 	dependencies: [
 		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0"),
