d6a2339e34171128775dddc298d55afb470c597c
Kyle Jessup <kyle.jessup@treefrog.ca>
2018-10-17 21:52:24 -0400

    Added further tests

--- Server/golang/golang.go
+++ Server/golang/golang.go
@@ -16,6 +16,8 @@ func Repeat(s string, count int) string {
 }
 
 var s1024 = []byte(Repeat("A", 1024))
+var s2048 = []byte(Repeat("A", 2048))
+var s4096 = []byte(Repeat("A", 4096))
 
 func handlerEmpty(w http.ResponseWriter, r *http.Request) {
     
@@ -25,8 +27,18 @@ func handler1024(w http.ResponseWriter, r *http.Request) {
     w.Write(s1024)
 }
 
+func handler2048(w http.ResponseWriter, r *http.Request) {
+    w.Write(s2048)
+}
+
+func handler4096(w http.ResponseWriter, r *http.Request) {
+    w.Write(s4096)
+}
+
 func main() {
     http.HandleFunc("/empty", handlerEmpty)
     http.HandleFunc("/1024", handler1024)
+    http.HandleFunc("/2048", handler2048)
+    http.HandleFunc("/4096", handler4096)
     log.Fatal(http.ListenAndServe(":8282", nil))
 }
