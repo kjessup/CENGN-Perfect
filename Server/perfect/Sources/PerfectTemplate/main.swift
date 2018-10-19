ca2298f8d029c0b08bfa82049aaff0d56a4fd777
Kyle Jessup <kyle.jessup@treefrog.ca>
2018-10-18 20:48:36 -0400

    trial

--- Server/perfect/Sources/PerfectTemplate/main.swift
+++ Server/perfect/Sources/PerfectTemplate/main.swift
@@ -25,7 +25,7 @@ var routes = Routes()
 let big1024 = String(repeating: "A", count: 1024)
 let big2048 = String(repeating: "A", count: 2048)
 let big4096 = String(repeating: "A", count: 4096)
-let big4096 = String(repeating: "A", count: 8192)
+let big8192 = String(repeating: "A", count: 8192)
 
 routes.add(uri: "/empty") {
 	req, resp in
