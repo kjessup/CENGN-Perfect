8fa4cd8116c7e3ce801503a3ee5f13205ff20e25
Kyle Jessup <kyle.jessup@treefrog.ca>
2018-10-23 11:43:07 -0400

    laravel

--- Server/laravel/routes/api.php
+++ Server/laravel/routes/api.php
@@ -13,6 +13,23 @@ use Illuminate\Http\Request;
 |
 */
 
+	$s1024 = str_repeat("A", 1024);
+	$s2048 = str_repeat("A", 2048);
+	$s4096 = str_repeat("A", 4096);
+	$s8192 = str_repeat("A", 8192);
+	
 Route::get('empty', function () {
 	return "";
+});
+Route::get('1024', function () {
+   return $s1024;
+});
+Route::get('2048', function () {
+   return $s2048;
+});
+Route::get('4096', function () {
+	return $s4096;
+});
+Route::get('8192', function () {
+   return $s8192;
 });
