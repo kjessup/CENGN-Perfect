58d8f9a83013443e9a425ac6c435b5d8ecd575d7
Kyle Jessup <kyle.jessup@treefrog.ca>
2018-10-24 14:25:42 -0400

    laravel

--- Server/laravel/app/Http/Kernel.php
+++ Server/laravel/app/Http/Kernel.php
@@ -35,12 +35,12 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ],
-
-        'api' => [
-            'throttle:60,1',
-            'bindings',
-        ],
+        ]
+//	,
+//        'api' => [
+//            'throttle:60,1',
+//            'bindings',
+//        ],
     ];
 
     /**
