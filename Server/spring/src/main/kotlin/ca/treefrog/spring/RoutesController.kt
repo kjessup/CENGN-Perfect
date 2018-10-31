a686268cdd63c551ba661e6e4cd02b2eea07e1b2
Kyle Jessup <kyle.jessup@treefrog.ca>
2018-10-31 13:06:56 -0400

    spring tests

--- Server/spring/src/main/kotlin/ca/treefrog/spring/RoutesController.kt
+++ Server/spring/src/main/kotlin/ca/treefrog/spring/RoutesController.kt
@@ -1,8 +1,10 @@
 package ca.treefrog.spring
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import org.springframework.web.multipart.MultipartFile
+import org.springframework.http.MediaType
+import org.springframework.util.MultiValueMap
+import org.springframework.web.multipart.MultipartHttpServletRequest
 
 var s1024 = "A".repeat(1024)
 var s2048 = "A".repeat(2048)
@@ -25,4 +27,36 @@ class RoutesController {
     
     @GetMapping("/8192")
     fun s8192() = s8192
+    
+    fun printArgs(body: Map<String,String>) {
+        val prefix = "abc"
+        for (char in 'a'..'z') {
+            val v = body[prefix + char]
+            print(v)
+        }
+    }
+
+    @GetMapping("/getArgs2048")
+    fun getArgs2048(@RequestParam queryParameters: Map<String, String>): String {
+        printArgs(queryParameters)
+        return s2048
+    }
+    
+    @RequestMapping(value="/postArgs2048", method=[RequestMethod.POST],
+                    consumes=[MediaType.APPLICATION_FORM_URLENCODED_VALUE])
+    fun postArgs2048(@RequestParam body: Map<String,String>): String {
+        printArgs(body)
+        return s2048
+    }
+    
+    @RequestMapping(value="/postArgsMulti2048", method=[RequestMethod.POST],
+                    consumes=[MediaType.MULTIPART_FORM_DATA_VALUE])
+    fun postArgsMulti2048(s: MultipartHttpServletRequest): String {
+         val prefix = "abc"
+        for (char in 'a'..'z') {
+            val v = s.getParameter(prefix + char)
+            print(v)
+        }
+        return s2048
+    }
 }
